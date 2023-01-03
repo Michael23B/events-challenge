13955cc3c86ea8c4853b1a90d420caf4ec52c543
Michael Burnett <michael23burnett@gmail.com>
2023-01-03 22:17:02 +1000

    Added logic to get events
    Added logic to get events and massaged response before sending it to frontend. We just need the pagination info and the actual events.

---
+++ frontend/src/app/shared/interfaces/get-events-query-string-parameters.ts
@@ -0,0 +1,8 @@
+export interface GetEventsQueryStringParameters {
+    countryCode?: string,
+    postalCode?: string,
+    startDateTime?: string,
+    endDateTime?: string,
+    page?: string,
+    size?: string
+}
--- server/src/handlers/get-all-items.js
+++ server/src/handlers/get-all-items.js
@@ -1,3 +1,6 @@
+const {httpsGet} = require('../common/http-request');
+const {createResponse} = require('../common/http-response');
+
 const DISCOVERY_API_URL = "https://app.ticketmaster.com/discovery/v2/";
 const EVENTS_RESOURCE = "events.json";
 const API_KEY = process.env.API_KEY;
@@ -35,4 +38,43 @@ const getAllowedQueryStringParameters = (event) => {
     const {countryCode, postalCode, startDateTime, endDateTime, page, size} = event.queryStringParameters;
 
     return {countryCode, postalCode, startDateTime, endDateTime, page, size};
+}
+
+/**
+ * Searches for events using the discovery API and provided query string parameters
+ * @param {Object} queryStringParameters Dictionary of query parameters.
+ * Key represents the parameter name and value represents the parameter value.
+ * @returns {Object} Object with pagination info and events list
+ */
+const getEvents = async (queryStringParameters) => {
+    // Add apikey to the query string parameters
+    const getEventsParameters = {...queryStringParameters, apikey: API_KEY};
+    const getEventsUrl = getUrlWithQueryParams(`${DISCOVERY_API_URL}${EVENTS_RESOURCE}`, getEventsParameters)
+
+    const eventsResponse = await httpsGet(getEventsUrl);
+
+    return {
+        page: eventsResponse.page,
+        events: eventsResponse._embedded.events
+    }
+}
+
+/**
+ * Returns a list of events from the TicketMaster discovery API
+ */
+exports.getAllItemsHandler = async (event) => {
+    console.debug('Received event: ', event);
+    let response = {};
+
+    try {
+        const queryStringParameters = getAllowedQueryStringParameters(event);
+        // Get events from the discovery API
+        const eventsResponse = await getEvents(queryStringParameters);
+        response = createResponse(200, eventsResponse);
+    } catch (err) {
+        console.error(err)
+        response = createResponse(500, {error: "Unable to call Discovery API"});
+    }
+
+    return response;
 }
